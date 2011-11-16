f7d643748f459904e73f4919c18ddf2626852838
David Kartik <david@kaskadia.com>
2011-11-16 15:25:59 -0800

    committing

--- pieces/bishop.php
+++ pieces/bishop.php
@@ -1,40 +1,53 @@
 <?php
+/*
+ * Class Name: Bishop
+ * Extends: Piece
+ */
 class Bishop extends Piece {
-  protected function MoveAllowed(Square $s) {
+
+  function __construct(Board $b, Colour $c, Square $s) {
+    parent::__construct($b, $c, $s);
+  }
+  
+  /*
+   * Name: moveAllowed
+   * Parameters:  Square $s: The Square that the piece is attempting to move to.
+   * Remarks:
+   *    - A delta of multiples of 9 is the bishop moving diagonally from top left to bottom right (or vice versa)
+   *    - A delta of multiples of 7 is the bishop moving diagonally from top right to bottom left (or vice versa)
+   */
+  protected function moveAllowed(Square $s) {
+    //Get the position difference between the current square and the desired square
     $delta = $s->index - $this->location->index;
+    
+    //Determine which direction the piece is moving, up or down the array
     if($delta > 0) {
       $direction = 1;
     } else {
       $direction = -1;
     }
 
-    if($this-board->HasSameColourPiece($s, $this->colour)) {
+    //A piece may not be able to move to another square with a piece of the same colour
+    if($this-board->hasSameColourPiece($s, $this->colour)) {
       return MoveType::ILLEGAL;
     }
 
+    // Moving from top left to bottom right
     if(abs($delta) % 9 == 0) {
-      if($this->board->SpacesBetweenEmpty($this->location->index, $delta / 9, 9, $direction)) {
+      if($this->board->spacesBetweenEmpty($this->location->index, $delta / 9, 9, $direction)) {
         return MoveType::NORMAL;
       }
     }
+    
+    // Moving from top right to bottom left
     if(abs($delta) % 7 == 0) {
-      if($this->SpacesBetweenEmpty($this->location->index, $delta / 7, 7, $direction)) {
+      if($this->spacesBetweenEmpty($this->location->index, $delta / 7, 7, $direction)) {
         return MoveType::NORMAL;
       }
     }
+    
+    //If the above cases do not satisfy, move is illegal
     return MoveType::ILLEGAL;
   }
-  
-  function __construct(Board $b, Colour $c, Square $s) {
-    parent::__construct($b, $c, $s);
-  }
-  
-  function TryMove(Square $s) {
-    $mt = $this->MoveAllowed($s);
-    if($mt != MoveType::ILLEGAL) {
-      $this->board->Move($this->location, $s);
-    }
-  }
-
 }
 ?>
